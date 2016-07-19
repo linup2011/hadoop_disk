e94cd6fcabfcb28e998acfe4544674904cd93756
jishi <jishi@dhcp-12-146.nay.redhat.com>
2016-07-20 00:24:43 +0800

    添加下载文件时由浏览器确定下载目录的功能

--- hadoop_old/src/com/controller/DownloadServlet.java
+++ hadoop_old/src/com/controller/DownloadServlet.java
@@ -5,8 +5,11 @@ import java.io.BufferedOutputStream;
 import java.io.File;  
 import java.io.FileInputStream;  
 import java.io.IOException;  
-import java.io.InputStream;  
-  
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;  
 import javax.servlet.ServletOutputStream;  
 import javax.servlet.http.HttpServlet;  
@@ -28,16 +31,38 @@ public class DownloadServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) 
      */  
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {  
-        String local = "/tmp/";  
+        String local = "/tmp/";
+        
         String filePath = new String(request.getParameter("filePath").getBytes("ISO-8859-1"),"GB2312");  
-        System.out.println(filePath);  
+        System.out.println("remote:"+filePath);  
+        System.out.println("local:"+local);
         JobConf conf = HdfsDAO.config();  
         HdfsDAO hdfs = new HdfsDAO(conf);  
-        hdfs.download(filePath, local);  
+        hdfs.download(filePath, local);
+        
+        String downfilename = filePath.substring( filePath.lastIndexOf("/")+1);
+        System.out.println("downfilename:"+downfilename);
+        File downfile = new File("/tmp/" + downfilename);
+      //设置响应头，控制浏览器下载该文件
+        response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode(downfilename, "UTF-8"));
+      //读取要下载的文件，保存到文件输入流
+        FileInputStream in = new FileInputStream("/tmp/"  + downfilename);
+      //创建输出流
+        OutputStream out = response.getOutputStream();
+        byte buffer[] = new byte[1024];
+        int len = 0;
+      //循环将输入流中的内容读取到缓冲区当中
+        while((len=in.read(buffer))>0){
+        	//输出缓冲区的内容到浏览器，实现文件下载
+        	out.write(buffer, 0, len);
+        }
+        in.close();
+        out.close();
           
-        FileStatus[] list = hdfs.ls("/");  
-        request.setAttribute("list",list);  
-        request.getRequestDispatcher("index.jsp").forward(request,response);   
+//        String username = (String) request.getSession().getAttribute("username");
+//        FileStatus[] list = hdfs.ls("/"+username);  
+//        request.setAttribute("list",list);  
+//        request.getRequestDispatcher("index.jsp").forward(request,response);   
     }  
   
     /** 
